a0bf330424d373cc43c9cd8d48d120254c1d8d57
saulmarin <sau7.mar1n@gmail.com>
2017-01-20 13:00:19 +0100

    Created ui files

---
+++ app/src/main/java/com/example/g7_1105ss/weatherworld/WeatherDetailFragment.java
@@ -0,0 +1,29 @@
+package com.example.g7_1105ss.weatherworld;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WeatherDetailFragment extends Fragment {
+
+
+    public WeatherDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_weather_detail, container, false);
+    }
+
+}
