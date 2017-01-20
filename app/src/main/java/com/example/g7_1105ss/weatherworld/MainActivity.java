5d12750778afdbbe9694169cc16534a098d99b1a
saulmarin <sau7.mar1n@gmail.com>
2017-01-20 09:55:48 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/g7_1105ss/weatherworld/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.g7_1105ss.weatherworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
